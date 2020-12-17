0757f60dac1bf43703991ecb4794bc800abd8589
TesterBaku <testerbaku@yahoo.com>
2020-12-17 15:00:31 -0800

    first commit

---
+++ src/main/java/com/qa/factory/DriverFactory.java
@@ -0,0 +1,61 @@
+package com.qa.factory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverFactory {
+	
+	
+	public WebDriver driver;
+	
+	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+	
+	
+	/**
+	 * This method is used to initialize the threadlocal driver on the basis of given browser
+	 * @param browser
+	 * @return tlDriver
+	 */
+	public WebDriver init_driver(String browser) {
+		
+		System.out.println("browser value is: " + browser);
+		
+		if(browser.equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			tlDriver.set(new ChromeDriver());
+		} 
+		else if(browser.equalsIgnoreCase("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			tlDriver.set(new FirefoxDriver());
+		} 
+		else if(browser.equalsIgnoreCase("Safari")) {
+			
+			tlDriver.set(new SafariDriver());
+		} 
+		else {
+			System.out.println("Please pass the correct browser value: " + browser);
+		}
+		
+		getDriver().manage().deleteAllCookies();
+		getDriver().manage().window().maximize();
+		return getDriver();
+	}
+	
+	/**
+	 * this method is used to get the driver with ThreadLocal
+	 * @return tlDriver
+	 */
+	public static synchronized WebDriver getDriver() {
+		return tlDriver.get();
+	}
+	
+	
+	
+	
+	
+
+}
