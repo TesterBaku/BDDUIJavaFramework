5a5a389d891e751fbb3d13176d1c64e496d4002a
TesterBaku <testerbaku@yahoo.com>
2020-12-22 18:41:19 -0800

    added junit parallell execution plug-in

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,65 +1,69 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "1ecba086-fb92-4bb1-95f8-714e91554643",
+    "id": "05670148-eb35-46bb-96ef-b17ca745e7d2",
     "feature": "Account Page Feature",
     "scenario": "Accounts page title",
-    "start": 1608676102195,
-    "group": 1,
+    "start": 1608690889836,
+    "group": 13,
     "content": "",
     "tags": "",
-    "end": 1608676181923,
+    "end": 1608690898812,
     "className": "passed"
   },
   {
-    "id": "39568c01-da0e-4cda-a7ca-9b2c10bb8ef1",
-    "feature": "Account Page Feature",
-    "scenario": "Accounts section count",
-    "start": 1608676181933,
-    "group": 1,
-    "content": "",
-    "tags": "",
-    "end": 1608676206069,
-    "className": "passed"
-  },
-  {
-    "id": "4086dfd4-da3a-4ab4-9a8f-b5ff1194b441",
-    "feature": "Login page feature",
-    "scenario": "Login page title",
-    "start": 1608676206075,
-    "group": 1,
-    "content": "",
-    "tags": "",
-    "end": 1608676215500,
-    "className": "passed"
-  },
-  {
-    "id": "a8234cf2-39d3-4ac5-944b-1795aa9dc4ac",
-    "feature": "Login page feature",
-    "scenario": "Forgot Password link",
-    "start": 1608676215505,
-    "group": 1,
-    "content": "",
-    "tags": "",
-    "end": 1608676222192,
-    "className": "passed"
-  },
-  {
-    "id": "f690db39-eb3e-4157-809c-70a108a3755b",
+    "id": "505984c1-89df-4993-96ca-95a1e27f0d0e",
     "feature": "Login page feature",
     "scenario": "Login with valid credentials",
-    "start": 1608676222198,
-    "group": 1,
+    "start": 1608690903893,
+    "group": 14,
     "content": "",
     "tags": "",
-    "end": 1608676229959,
+    "end": 1608690910875,
+    "className": "passed"
+  },
+  {
+    "id": "aa554e68-1512-4f26-ae2b-0c30b3a1c8d5",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1608690889837,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1608690897158,
+    "className": "passed"
+  },
+  {
+    "id": "41a81ab3-1d9e-457c-b484-d03fac7f3926",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1608690897166,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1608690903887,
+    "className": "passed"
+  },
+  {
+    "id": "ea916e15-242f-4bfb-a466-a78f562c82bb",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1608690898821,
+    "group": 13,
+    "content": "",
+    "tags": "",
+    "end": 1608690906793,
     "className": "passed"
   }
 ]);
 CucumberHTML.timelineGroups.pushArray([
   {
-    "id": 1,
-    "content": "Thread[main,5,main]"
+    "id": 13,
+    "content": "Thread[pool-1-thread-1,5,main]"
+  },
+  {
+    "id": 14,
+    "content": "Thread[pool-1-thread-2,5,main]"
   }
 ]);
 });
